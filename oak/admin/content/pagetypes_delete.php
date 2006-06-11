1061f6db02ae232069b576bf044ec695b4c0aaa4
Olaf Gleba <og@welcompose.de>
2006-06-11 15:14:46 +0000

    Forms and class for page type management added

---
+++ oak/admin/content/pagetypes_delete.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * Project: Oak
+ * File: pagetypes_delete.php
+ *
+ * Copyright (c) 2006 sopic GmbH
+ *
+ * Project owner:
+ * sopic GmbH
+ * 8472 Seuzach, Switzerland
+ * http://www.sopic.com/
+ *
+ * This file is licensed under the terms of the Open Software License
+ * http://www.opensource.org/licenses/osl-2.1.php
+ *
+ * $Id$
+ *
+ * @copyright 2006 sopic GmbH
+ * @author Andreas Ahlenstorf
+ * @package Oak
+ * @license http://www.opensource.org/licenses/osl-2.1.php Open Software License
+ */
+
+// get loader
+$path_parts = array(
+	dirname(__FILE__),
+	'..',
+	'..',
+	'core',
+	'loader.php'
+);
+$loader_path = implode(DIRECTORY_SEPARATOR, $path_parts);
+require($loader_path);
+
+// start base
+/* @var $BASE base */
+$BASE = load('base:base');
+
+// deregister globals
+$deregister_globals_path = dirname(__FILE__).'/../../core/includes/deregister_globals.inc.php';
+require(Base_Compat::fixDirectorySeparator($deregister_globals_path));
+
+try {
+	// start output buffering
+	@ob_start();
+	
+	// load smarty
+	$smarty_admin_conf = dirname(__FILE__).'/../../core/conf/smarty_admin.inc.php';
+	$BASE->utility->loadSmarty(Base_Compat::fixDirectorySeparator($smarty_admin_conf), true);
+	
+	// load gettext
+	$gettext_path = dirname(__FILE__).'/../../core/includes/gettext.inc.php';
+	include(Base_Compat::fixDirectorySeparator($gettext_path));
+	gettextInitSoftware($BASE->_conf['locales']['all']);
+	
+	// start session
+	/* @var $SESSION session */
+	$SESSION = load('base:session');
+
+	// load user class
+	/* @var $USER Application_User */
+	$USER = load('application:user');
+	
+	// load project class
+	/* @var $PROJECT Application_Project */
+	$PROJECT = load('application:project');
+	
+	// load pagetype class
+	/* @var $PAGETYPE Content_Pagetype */
+	$PAGETYPE = load('content:pagetype');
+		
+	// init user and project
+	$USER->initUserAdmin();
+	$PROJECT->initProjectAdmin(OAK_CURRENT_USER);
+	
+	// assign current user and project id
+	$BASE->utility->smarty->assign('oak_current_user', OAK_CURRENT_USER);
+	$BASE->utility->smarty->assign('oak_current_project', OAK_CURRENT_PROJECT);
+
+	try {
+		// start transaction
+		$BASE->db->begin();
+		
+		// delete row
+		$PAGETYPE->deletePageType(BASE_Cnc::filterRequest($_REQUEST['id'], OAK_REGEX_NUMERIC));
+		
+		// commit transaction
+		$BASE->db->commit();
+	} catch (Exception $e) {
+		// do rollback
+		$BASE->db->rollback();
+		
+		// re-throw exception
+		throw $e;
+	}
+
+	// clean buffer
+	if (!$BASE->debug_enabled()) {
+		@ob_end_clean();
+	}
+
+	// go back to overview page
+	header("Location: pagetypes_select.php");
+	exit;
+
+} catch (Exception $e) {
+	// clean buffer
+	if (!$BASE->debug_enabled()) {
+		@ob_end_clean();
+	}
+	
+	// raise error
+	Base_Error::triggerException($BASE->utility->smarty, $e);	
+	
+	// exit
+	exit;
+}
+?>
