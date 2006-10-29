149de045ca04e454caf182ff39eb20275121a9b2
Olaf Gleba <og@welcompose.de>
2006-10-29 22:53:26 +0000

    Some reformatting done

--- oak/admin/content/podcastcategories_add.php
+++ oak/admin/content/podcastcategories_add.php
@@ -127,19 +127,19 @@ try {
 		$BASE->utility->smarty->assign('oak_admin_root_www',
 			$BASE->_conf['path']['oak_admin_root_www']);
 		
-	    // build session
-	    $session = array(
+		// build session
+		$session = array(
 			'response' => Base_Cnc::filterRequest($_SESSION['response'], OAK_REGEX_NUMERIC)
-	    );
-	    
-	    // assign prepared session array to smarty
-	    $BASE->utility->smarty->assign('session', $session);
-	    
-	    // empty $_SESSION
-	    if (!empty($_SESSION['response'])) {
-	        $_SESSION['response'] = '';
-	    }
-	    
+		);
+		
+		// assign prepared session array to smarty
+		$BASE->utility->smarty->assign('session', $session);
+		
+		// empty $_SESSION
+		if (!empty($_SESSION['response'])) {
+			$_SESSION['response'] = '';
+		}
+		
 		// assign current user and project id
 		$BASE->utility->smarty->assign('oak_current_user', OAK_CURRENT_USER);
 		$BASE->utility->smarty->assign('oak_current_project', OAK_CURRENT_PROJECT);
