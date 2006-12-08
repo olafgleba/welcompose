37d05c03b5802a61978bc90e8feb3e9e26a7be7a
Olaf Gleba <og@welcompose.de>
2006-12-08 20:01:07 +0000

    Build script updated

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,7 +14,7 @@ fi
 rm -rf wcom-trunk
 
 # create package output directory
-mkdir "wcom-$BUILD_NUMBER"
+mkdir "welcompose-$BUILD_NUMBER"
 
 # export trunk
 svn export https://www.dotthink.net/svn/Welcompose/trunk wcom-trunk
@@ -23,26 +23,26 @@ svn export https://www.dotthink.net/svn/Welcompose/trunk wcom-trunk
 cd wcom-trunk
 
 # move database schema to setup directory
-cp database/wcom.sql wcom/setup/wcom.sql
+cp database/wcom.sql welcompose/setup/wcom.sql
 
 # replace sys.inc.php with sys.inc.php-dist
-mv wcom/core/conf/sys.inc.php-dist wcom/core/conf/sys.inc.php
+mv welcompose/core/conf/sys.inc.php-dist welcompose/core/conf/sys.inc.php
 
 # remove database & doc directory
 rm -rf database
 rm -rf documentation
 
 # create source packages
-tar cvfz "../wcom-$BUILD_NUMBER/wcom-$BUILD_NUMBER-src".tar.gz wcom
-tar cvfj "../wcom-$BUILD_NUMBER/wcom-$BUILD_NUMBER-src".tar.bz2 wcom
-zip -r "../wcom-$BUILD_NUMBER/wcom-$BUILD_NUMBER-src" wcom
+tar cvfz "../welcompose-$BUILD_NUMBER/welcompose-$BUILD_NUMBER-src".tar.gz welcompose
+tar cvfj "../welcompose-$BUILD_NUMBER/welcompose-$BUILD_NUMBER-src".tar.bz2 welcompose
+zip -r "../welcompose-$BUILD_NUMBER/welcompose-$BUILD_NUMBER-src" welcompose
 
 # create compressed package
 echo "Creating compressed package"
 php scripts/installer/create_install_package.php \
 	--compress=true \
 	--package-extractor-script=scripts/installer/installer.inc.php \
-	--software-directory=wcom \
+	--software-directory=welcompose \
 	--output-file=install-zlib.php \
 	--installer-type=web \
 	--web-installer-dir=scripts/installer/web_installer
@@ -52,25 +52,25 @@ echo "Creating uncompressed package"
 php scripts/installer/create_install_package.php \
 	--compress=false \
 	--package-extractor-script=scripts/installer/installer.inc.php \
-	--software-directory=wcom \
+	--software-directory=welcompose \
 	--output-file=install.php \
 	--installer-type=web \
 	--web-installer-dir=scripts/installer/web_installer
 
-# remove wcom directory
-rm -rf wcom
+# remove welcompose directory
+rm -rf welcompose
 
-# create wcom directory
-mkdir wcom
+# create welcompose directory
+mkdir welcompose
 
-# move installer files to wcom directory
-mv install-zlib.php wcom
-mv install.php wcom
+# move installer files to welcompose directory
+mv install-zlib.php welcompose
+mv install.php welcompose
 
 # create tarball (gzipped and bzipped) & zip file
-tar cvfz "../wcom-$BUILD_NUMBER/wcom-$BUILD_NUMBER".tar.gz wcom
-tar cvfj "../wcom-$BUILD_NUMBER/wcom-$BUILD_NUMBER".tar.bz2 wcom
-zip -r "../wcom-$BUILD_NUMBER/wcom-$BUILD_NUMBER" wcom
+tar cvfz "../welcompose-$BUILD_NUMBER/welcompose-$BUILD_NUMBER".tar.gz welcompose
+tar cvfj "../welcompose-$BUILD_NUMBER/welcompose-$BUILD_NUMBER".tar.bz2 welcompose
+zip -r "../welcompose-$BUILD_NUMBER/welcompose-$BUILD_NUMBER" welcompose
 
 # leave wcom-trunk directory
 cd ../
