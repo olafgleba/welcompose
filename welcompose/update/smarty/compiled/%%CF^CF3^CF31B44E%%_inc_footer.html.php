d4534ba22d77835d2b03b470c4c50b2be632938c
Olaf Gleba <og@welcompose.de>
2007-02-12 20:38:15 +0000

    Removing accidentally imported compiled templates

--- welcompose/update/smarty/compiled/%%CF^CF3^CF31B44E%%_inc_footer.html.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.14, created on 2007-02-05 14:39:15
-         compiled from _inc_footer.html */ ?>
-<p class="copyright">
-<span class="fright">Licensed below the terms of the <a href="http://www.opensource.org/licenses/osl-3.0.php">Open 
-Software License 3.0</a></span>
-&copy; 2006 - 2007 <a href="http://www.welcompose.com/"><strong>Welcompose<sup>&reg;</sup></strong></a> Version 0.8 &ndash; powered by <a href="http://www.sopic.com/"><strong>Sopic<sup>&reg;</sup></strong></a>
-</p>
