564becf79dbbb52e6c8c7ef8cba5f7f6a1296a9a
Olaf Gleba <og@welcompose.de>
2007-02-15 15:51:34 +0000

    More update tasks added (0.6 to 0.8)

---
+++ welcompose/update/tasks/0001-003.php
@@ -0,0 +1,170 @@
+<?php
+
+/**
+ * Project: Welcompose
+ * File: 0001-003.php
+ *
+ * Copyright (c) 2006 sopic GmbH
+ *
+ * Project owner:
+ * sopic GmbH
+ * 8472 Seuzach, Switzerland
+ * http://www.sopic.com/
+ *
+ * This file is licensed under the terms of the Open Software License 3.0
+ * http://www.opensource.org/licenses/osl-3.0.php
+ *
+ * $Id$
+ *
+ * @copyright 2006 sopic GmbH
+ * @author Andreas Ahlenstorf
+ * @package Welcompose
+ * @license http://www.opensource.org/licenses/osl-3.0.php Open Software License 3.0
+ */
+
+// get loader
+$path_parts = array(
+	dirname(__FILE__),
+	'..',
+	'..',
+	'core',
+	'loader.php'
+);
+$loader_path = implode(DIRECTORY_SEPARATOR, $path_parts);
+require($loader_path);
+
+// start base
+/* @var $BASE base */
+$BASE = load('base:base');
+
+// deregister globals
+$deregister_globals_path = dirname(__FILE__).'/../../core/includes/deregister_globals.inc.php';
+require(Base_Compat::fixDirectorySeparator($deregister_globals_path));
+
+try {
+	// start output buffering
+	@ob_start();
+	
+	// load smarty
+	$smarty_update_conf = dirname(__FILE__).'/../smarty.inc.php';
+	$BASE->utility->loadSmarty(Base_Compat::fixDirectorySeparator($smarty_update_conf), true);
+	
+	// load gettext
+	$gettext_path = dirname(__FILE__).'/../../core/includes/gettext.inc.php';
+	include(Base_Compat::fixDirectorySeparator($gettext_path));
+	gettextInitSoftware($BASE->_conf['locales']['all']);
+	
+	// start Base_Session
+	/* @var $SESSION session */
+	$SESSION = load('base:session');
+	
+	// connect to database
+	$BASE->loadClass('database');
+	
+	// define major/minor task number
+	define('TASK_MAJOR', '0001');
+	define('TASK_MINOR', '003');
+	
+	// get schema version from database
+	$sql = "
+		SELECT
+			`schema_version`
+		FROM
+			".WCOM_DB_APPLICATION_INFO."
+		LIMIT
+			1
+	";
+	$version = $BASE->db->select($sql, 'field');
+	list($major, $minor) = explode('-', $version);
+	
+	/*
+	 * References
+	 * ----------
+	 *
+	 * Changeset: 832
+	 * Ticket: 9
+	 * 
+	 * Changes to be applied
+	 * ---------------------
+	 *
+	 * - Add fields alternate_name, description and optional_text to content_pages
+	 *   `alternate_name` varchar(255)
+	 *   `description` text
+	 *   `optional_text` text
+	 */
+	if ($major < TASK_MAJOR || ($major == TASK_MAJOR && $minor < TASK_MINOR)) {
+		try {
+			// begin transaction
+			$BASE->db->begin();
+		
+			// add new fields
+			$sql = "
+				ALTER TABLE
+					".WCOM_DB_CONTENT_PAGES."
+				ADD
+					`alternate_name` varchar(255)
+				AFTER
+					`name_url`
+			";
+			$BASE->db->execute($sql);
+			
+			$sql = "
+				ALTER TABLE
+					".WCOM_DB_CONTENT_PAGES."
+				ADD
+					`description` text
+				AFTER
+					`alternate_name`
+			";
+			$BASE->db->execute($sql);
+		
+			$sql = "
+				ALTER TABLE
+					".WCOM_DB_CONTENT_PAGES."
+				ADD
+					`optional_text` text
+				AFTER
+					`description`
+			";
+			$BASE->db->execute($sql);
+		
+			// update schema version
+			$sqlData = array(
+				'schema_version' => TASK_MAJOR.'-'.TASK_MINOR
+			);
+			
+			$BASE->db->update(WCOM_DB_APPLICATION_INFO, $sqlData);
+			
+			// commit
+			$BASE->db->commit();
+		} catch (Exception $e) {
+			// do rollback
+			$BASE->db->rollback();
+		
+			// re-throw exception
+			throw $e;
+		}
+	}
+	
+	// display the form
+	define("WCOM_TEMPLATE_KEY", md5($_SERVER['REQUEST_URI']));
+	$BASE->utility->smarty->display('tasks/0001-003.html', WCOM_TEMPLATE_KEY);
+	
+	// flush the buffer
+	@ob_end_flush();
+	
+	exit;
+} catch (Exception $e) {
+	// clean the buffer
+	if (!$BASE->debug_enabled()) {
+		@ob_end_clean();
+	}
+	
+	// raise error
+	Base_Error::triggerException($BASE->utility->smarty, $e);
+	
+	// exit
+	exit;
+}
+
+?>
---
+++ welcompose/update/tasks/0001-004.php
@@ -0,0 +1,146 @@
+<?php
+
+/**
+ * Project: Welcompose
+ * File: 0001-004.php
+ *
+ * Copyright (c) 2006 sopic GmbH
+ *
+ * Project owner:
+ * sopic GmbH
+ * 8472 Seuzach, Switzerland
+ * http://www.sopic.com/
+ *
+ * This file is licensed under the terms of the Open Software License 3.0
+ * http://www.opensource.org/licenses/osl-3.0.php
+ *
+ * $Id$
+ *
+ * @copyright 2006 sopic GmbH
+ * @author Andreas Ahlenstorf
+ * @package Welcompose
+ * @license http://www.opensource.org/licenses/osl-3.0.php Open Software License 3.0
+ */
+
+// get loader
+$path_parts = array(
+	dirname(__FILE__),
+	'..',
+	'..',
+	'core',
+	'loader.php'
+);
+$loader_path = implode(DIRECTORY_SEPARATOR, $path_parts);
+require($loader_path);
+
+// start base
+/* @var $BASE base */
+$BASE = load('base:base');
+
+// deregister globals
+$deregister_globals_path = dirname(__FILE__).'/../../core/includes/deregister_globals.inc.php';
+require(Base_Compat::fixDirectorySeparator($deregister_globals_path));
+
+try {
+	// start output buffering
+	@ob_start();
+	
+	// load smarty
+	$smarty_update_conf = dirname(__FILE__).'/../smarty.inc.php';
+	$BASE->utility->loadSmarty(Base_Compat::fixDirectorySeparator($smarty_update_conf), true);
+	
+	// load gettext
+	$gettext_path = dirname(__FILE__).'/../../core/includes/gettext.inc.php';
+	include(Base_Compat::fixDirectorySeparator($gettext_path));
+	gettextInitSoftware($BASE->_conf['locales']['all']);
+	
+	// start Base_Session
+	/* @var $SESSION session */
+	$SESSION = load('base:session');
+	
+	// connect to database
+	$BASE->loadClass('database');
+	
+	// define major/minor task number
+	define('TASK_MAJOR', '0001');
+	define('TASK_MINOR', '004');
+	
+	// get schema version from database
+	$sql = "
+		SELECT
+			`schema_version`
+		FROM
+			".WCOM_DB_APPLICATION_INFO."
+		LIMIT
+			1
+	";
+	$version = $BASE->db->select($sql, 'field');
+	list($major, $minor) = explode('-', $version);
+	
+	/*
+	 * References
+	 * ----------
+	 *
+	 * Changeset: 833
+	 * Ticket: 6
+	 * 
+	 * Changes to be applied
+	 * ---------------------
+	 *
+	 * - Add unique key around content_boxes.page and content_boxes.name
+	 *   UNIQUE INDEX `unique_name_per_page`(`page`, `name`)
+	 */
+	if ($major < TASK_MAJOR || ($major == TASK_MAJOR && $minor < TASK_MINOR)) {
+		try {
+			// begin transaction
+			$BASE->db->begin();
+		
+			// add new index
+			$sql = "
+				ALTER TABLE
+					".WCOM_DB_CONTENT_BOXES."
+				ADD
+					UNIQUE INDEX `unique_name_per_page`(`page`, `name`)
+			";
+			$BASE->db->execute($sql);
+		
+			// update schema version
+			$sqlData = array(
+				'schema_version' => TASK_MAJOR.'-'.TASK_MINOR
+			);
+			
+			$BASE->db->update(WCOM_DB_APPLICATION_INFO, $sqlData);
+			
+			// commit
+			$BASE->db->commit();
+		} catch (Exception $e) {
+			// do rollback
+			$BASE->db->rollback();
+		
+			// re-throw exception
+			throw $e;
+		}
+	}
+	
+	// display the form
+	define("WCOM_TEMPLATE_KEY", md5($_SERVER['REQUEST_URI']));
+	$BASE->utility->smarty->display('tasks/0001-004.html', WCOM_TEMPLATE_KEY);
+	
+	// flush the buffer
+	@ob_end_flush();
+	
+	exit;
+} catch (Exception $e) {
+	// clean the buffer
+	if (!$BASE->debug_enabled()) {
+		@ob_end_clean();
+	}
+	
+	// raise error
+	Base_Error::triggerException($BASE->utility->smarty, $e);
+	
+	// exit
+	exit;
+}
+
+?>
