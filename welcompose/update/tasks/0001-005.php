1fff64ac8cdcfc3f1bce5396e6bcd20eead7ef9b
Olaf Gleba <og@welcompose.de>
2007-02-22 12:45:17 +0000

    Next move

--- welcompose/update/tasks/0001-005.php
+++ welcompose/update/tasks/0001-005.php
@@ -2,7 +2,7 @@
 
 /**
  * Project: Welcompose
- * File: 0001-004.php
+ * File: 0001-005.php
  *
  * Copyright (c) 2006 sopic GmbH
  *
@@ -63,7 +63,7 @@ try {
 	
 	// define major/minor task number
 	define('TASK_MAJOR', '0001');
-	define('TASK_MINOR', '004');
+	define('TASK_MINOR', '005');
 	
 	// get schema version from database
 	$sql = "
@@ -115,7 +115,7 @@ try {
 			$BASE->db->commit();
 			
 			// assign task number
-			$BASE->utility->smarty->assign('task', '0001-004');
+			$BASE->utility->smarty->assign('task', '0001-005');
 		} catch (Exception $e) {
 			// do rollback
 			$BASE->db->rollback();
@@ -127,7 +127,7 @@ try {
 	
 	// display the form
 	define("WCOM_TEMPLATE_KEY", md5($_SERVER['REQUEST_URI']));
-	$BASE->utility->smarty->display('tasks/0001-004.html', WCOM_TEMPLATE_KEY);
+	$BASE->utility->smarty->display('tasks/0001-005.html', WCOM_TEMPLATE_KEY);
 	
 	// flush the buffer
 	@ob_end_flush();
