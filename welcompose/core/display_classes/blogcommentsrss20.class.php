0128913776ac4d1cb961b55ecea9166bf1b44051
Olaf Gleba <og@welcompose.de>
2007-02-23 17:04:29 +0000

    Fixed ticket:27

---
+++ welcompose/core/display_classes/blogcommentsrss20.class.php
@@ -0,0 +1,216 @@
+<?php
+
+/**
+ * Project: Welcompose
+ * File: blogcommentsrss20.class.php
+ * 
+ * Copyright (c) 2006 sopic GmbH
+ * 
+ * Project owner:
+ * sopic GmbH
+ * 8472 Seuzach, Switzerland
+ * http://www.sopic.com/
+ *
+ * This file is licensed under the terms of the Open Software License 3.0
+ * http://www.opensource.org/licenses/osl-3.0.php
+ * 
+ * $Id: blogrss20.class.php 819 2007-01-19 15:48:05Z andreas $
+ * 
+ * @copyright 2006 sopic GmbH
+ * @author Andreas Ahlenstorf
+ * @package Welcompose
+ * @license http://www.opensource.org/licenses/osl-3.0.php Open Software License 3.0
+ */
+
+// load the display interface
+if (!interface_exists('Display')) {
+	$path_parts = array(
+		dirname(__FILE__),
+		'display.interface.php'
+	);
+	require(implode(DIRECTORY_SEPARATOR, $path_parts));
+}
+
+/**
+ * Class loader compatible to loader.php. Wrapps around constructor.
+ * 
+ * @param array
+ * @return object
+ */
+function Display_BlogCommentsRss20 ($args)
+{
+	// check input
+	if (!is_array($args)) {
+		trigger_error('Constructor args are not an array', E_USER_ERROR);
+	}
+	if (!array_key_exists(0, $args)) {
+		trigger_error('Constructor arg project does not exist', E_USER_ERROR);
+	}
+	if (!array_key_exists(1, $args)) {
+		trigger_error('Constructor arg page does not exist', E_USER_ERROR);
+	}
+
+	return new Display_BlogCommentsRss20($args[0], $args[1]);
+}
+
+class Display_BlogCommentsRss20 implements Display {
+	
+	/**
+	 * Reference to base class
+	 *
+	 * @var object
+	 */
+	public $base = null;
+	
+	/**
+	 * Container for project information
+	 * 
+	 * @var array
+	 */
+	protected $_project = array();
+	
+	/**
+	 * Container for page information
+	 * 
+	 * @var array
+	 */
+	protected $_page = array();
+	
+/**
+ * Creates new instance of display driver. Takes an array
+ * with the project information as first argument, an array
+ * with the information about the current page as second
+ * argument.
+ * 
+ * @throws Display_BlogCommentsRss20Exception
+ * @param array Project information
+ * @param array Page information
+ */
+public function __construct($project, $page)
+{
+	try {
+		// get base instance
+		$this->base = load('base:base');
+		
+		// establish database connection
+		$this->base->loadClass('database');
+				
+	} catch (Exception $e) {
+		
+		// trigger error
+		printf('%s on Line %u: Unable to start base class. Reason: %s.', $e->getFile(),
+			$e->getLine(), $e->getMessage());
+		exit;
+	}
+	
+	// input check
+	if (!is_array($project)) {
+		throw new Display_BlogCommentsRss20Exception("Input for parameter project is expected to be an array");
+	}
+	if (!is_array($page)) {
+		throw new Display_BlogCommentsRss20Exception("Input for parameter page is expected to be an array");
+	}
+	
+	// assign project, page info to class properties
+	$this->_project = $project;
+	$this->_page = $page;
+}
+
+/**
+ * Default method that will be called from the display script
+ * and has to care about the page preparation. Returns boolean
+ * true on success.
+ * 
+ * @return bool
+ */ 
+public function render ()
+{
+	// nothing to do
+	return true;
+}
+
+/**
+ * Returns the cache mode for the current template.
+ * 
+ * @return int
+ */
+public function getMainTemplateCacheMode ()
+{
+	return 0;
+}
+
+/**
+ * Returns the cache lifetime of the current template.
+ * 
+ * @return int
+ */
+public function getMainTemplateCacheLifetime ()
+{
+	return 0;
+}
+
+/** 
+ * Returns the name of the current template.
+ * 
+ * @return string
+ */ 
+public function getMainTemplateName ()
+{
+	return "wcom:blog_comments_rss20.".WCOM_CURRENT_PAGE;
+}
+
+/**
+ * Returns the redirect location of the the current
+ * document (~ $PHP_SELF without it's problems) with the
+ * Location: header prepended.
+ * 
+ * @return string
+ */
+public function getRedirectLocationSelf ()
+{
+	return "Location: ".$this->getLocationSelf();
+}
+
+/**
+ * Returns the redirect location of the the current
+ * document (~ $PHP_SELF without it's problems).
+ * 
+ * @return string
+ */
+public function getLocationSelf ()
+{
+	// prepare params
+	$params = array(
+		'page_id' => $this->_page['id'],
+		'action' => 'CommentsRss20'
+	);
+	
+	// send params to url generator. we hope to get back something useful.
+	$URLGENERATOR = load('Utility:UrlGenerator');
+	$url = $URLGENERATOR->generateInternalLink($params);
+	
+	// return the url or a hash mark if the url is empty 
+	if (empty($url)) {
+		return '#';
+	} else {
+		return $url;
+	}
+}
+
+/**
+ * Returns information whether to skip authentication
+ * or not.
+ * 
+ * @return bool
+ */
+public function skipAuthentication ()
+{
+	return false;
+}
+
+// end of class
+}
+
+class Display_BlogCommentsRss20Exception extends Exception { }
+
+?>
