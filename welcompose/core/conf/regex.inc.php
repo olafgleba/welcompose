5b912e530402265ab06b537359ea45cb451a62f3
Olaf Gleba <og@welcompose.de>
2007-06-07 21:10:34 +0000

    Double usage of 'D' modifier fixed (introduced with changeset:1032 where I applied a bad regular expression to change the modifiers).

--- welcompose/core/conf/regex.inc.php
+++ welcompose/core/conf/regex.inc.php
@@ -23,49 +23,49 @@
  */
 
 // Defines regular expression for numbers/numeric strings
-define("WCOM_REGEX_NUMERIC", "=^([0-9]+)$=DD");
+define("WCOM_REGEX_NUMERIC", "=^([0-9]+)$=D");
 
 // Defines regular expression for alphanumeric strings
-define("WCOM_REGEX_ALPHANUMERIC", "=^([0-9a-z]+)$=DDi");
+define("WCOM_REGEX_ALPHANUMERIC", "=^([0-9a-z]+)$=Di");
 
 // Defines regular expression for the article number
-define("WCOM_REGEX_ARTICLE_NUMBER", "=^([-_,;\.:#+*\\/\w\s]+)$=DDi");
+define("WCOM_REGEX_ARTICLE_NUMBER", "=^([-_,;\.:#+*\\/\w\s]+)$=Di");
 
 // Defines regular expression for URLs
-define("WCOM_REGEX_URL", "=^((ht|f)tp(s?))\://([0-9a-z\-]+\.)+[a-z]{2,6}(\:[0-9]+)?(/\S*)?$=DDi");
+define("WCOM_REGEX_URL", "=^((ht|f)tp(s?))\://([0-9a-z\-]+\.)+[a-z]{2,6}(\:[0-9]+)?(/\S*)?$=Di");
 
 // Defines regular expression for the template type
 // names
-define("WCOM_REGEX_TEMPLATE_TYPE_NAME", "=^([a-z0-9_]+)$=DDi");
+define("WCOM_REGEX_TEMPLATE_TYPE_NAME", "=^([a-z0-9_]+)$=Di");
 
 // Defines regular expression for the template set
 // names
-define("WCOM_REGEX_TEMPLATE_SET_NAME", "=^([a-z0-9_]+)$=DDi");
+define("WCOM_REGEX_TEMPLATE_SET_NAME", "=^([a-z0-9_]+)$=Di");
 
 // Defines regular expression for the template
 // resource names
-define("WCOM_REGEX_TEMPLATE_RESOURCE", "=^([a-z0-9-_]+).([0-9]+)$=DDi");
+define("WCOM_REGEX_TEMPLATE_RESOURCE", "=^([a-z0-9-_]+).([0-9]+)$=Di");
 
 // Defines regular expression for strings containing
 // the name of a Smarty variable (only for usage
 // in Smarty plugins).
-define("WCOM_REGEX_SMARTY_VAR_NAME", "=^([a-z]|_){1}([0-9a-z_]+)$=DDi");
+define("WCOM_REGEX_SMARTY_VAR_NAME", "=^([a-z]|_){1}([0-9a-z_]+)$=Di");
 
 // Defines regular expression for strings containing
 // the name of a namespace (only for usage in Smarty plugins).
-define("WCOM_REGEX_SMARTY_NS_NAME", "=^([a-z]|_){1}([0-9a-z_]+)$=DDi");
+define("WCOM_REGEX_SMARTY_NS_NAME", "=^([a-z]|_){1}([0-9a-z_]+)$=Di");
 
 // Defines regular expression for strings containing
 // the name of a class (only for usage in Smarty plugins).
-define("WCOM_REGEX_SMARTY_CLASS_NAME", "=^([a-z]|_){1}([0-9a-z_]+)$=DDi");
+define("WCOM_REGEX_SMARTY_CLASS_NAME", "=^([a-z]|_){1}([0-9a-z_]+)$=Di");
 
 // Defines regular expression for strings containing
 // the name of a class' method (only for usage in Smarty plugins).
-define("WCOM_REGEX_SMARTY_METHOD_NAME", "=^([a-z]|_){1}([0-9a-z_]+)$=DDi");
+define("WCOM_REGEX_SMARTY_METHOD_NAME", "=^([a-z]|_){1}([0-9a-z_]+)$=Di");
 
 // Defines regular expression for strings containing
 // one or more order macros.
-define("WCOM_REGEX_ORDER_MACRO", "=^((([A-Z-_]+)(:DESC|:ASC|)(;(?!$)|$))+)$=DD");
+define("WCOM_REGEX_ORDER_MACRO", "=^((([A-Z-_]+)(:DESC|:ASC|)(;(?!$)|$))+)$=D");
 
 // Defines regular expression to be used to remove 
 // undesired chars when determining the first char of a tag
@@ -73,70 +73,70 @@ define("WCOM_REGEX_TAG_FIRST_CHAR_CLEANUP", "=[^a-z0-9]=");
 
 // Defines regular expression for strings containing a
 // page type
-define("WCOM_REGEX_PAGE_TYPE", "=^([A-Z0-9]+)_([A-Z0-9_]+)$=DD");
+define("WCOM_REGEX_PAGE_TYPE", "=^([A-Z0-9]+)_([A-Z0-9_]+)$=D");
 
 // Defines regular expression for strings containing a
 // page type name
-define("WCOM_REGEX_PAGE_TYPE_NAME", "=^(?!WCOM)([A-Z0-9]+)_([A-Z0-9_]+)$=DD");
+define("WCOM_REGEX_PAGE_TYPE_NAME", "=^(?!WCOM)([A-Z0-9]+)_([A-Z0-9_]+)$=D");
 
 // Defines regular expression for strings containing an
 // internal page type name
-define("WCOM_REGEX_PAGE_TYPE_INTERNAL_NAME", "=^(([A-Z]{1}[a-z]{1,})+)$=DD");
+define("WCOM_REGEX_PAGE_TYPE_INTERNAL_NAME", "=^(([A-Z]{1}[a-z]{1,})+)$=D");
 
 // Defines regular expression for strings containing a
 // field id name
-define("WCOM_REGEX_FORM_FIELD_ID", "=^([a-z0-9-_]+)$=DDi");
+define("WCOM_REGEX_FORM_FIELD_ID", "=^([a-z0-9-_]+)$=Di");
 
 // Defines regular expression for non empty strings
-define("WCOM_REGEX_NON_EMPTY", "=^.+$=DD");
+define("WCOM_REGEX_NON_EMPTY", "=^.+$=D");
 
 // Defines regular expression for the help template
 // names
-define("WCOM_REGEX_HELP", "=^([a-z0-9-_]+)$=DDi");
+define("WCOM_REGEX_HELP", "=^([a-z0-9-_]+)$=Di");
 
 // Defines regular expression for javascript file
 // names (exclude thirdparty javascript files)
-define("WCOM_REGEX_JS", '=^(wcom+)\.([a-z_]+)\.js$=DDi');
+define("WCOM_REGEX_JS", '=^(wcom+)\.([a-z_]+)\.js$=Di');
 
 // Defines regular expression for setup javascript file
 // names (exclude thirdparty javascript files)
-define("WCOM_REGEX_SETUP_JS", '=^(wcom.setup+)\.([a-z_]+)\.js$=DDi');
+define("WCOM_REGEX_SETUP_JS", '=^(wcom.setup+)\.([a-z_]+)\.js$=Di');
 
 // Defines regular expression for setup javascript file
 // names (exclude thirdparty javascript files)
-define("WCOM_REGEX_UPDATE_JS", '=^(wcom.update+)\.([a-z_]+)\.js$=DDi');
+define("WCOM_REGEX_UPDATE_JS", '=^(wcom.update+)\.([a-z_]+)\.js$=Di');
 
 // Defines regular expression for strings containing
 // a file name of a php script
-define("WCOM_REGEX_FILE_NAME_PHP", '=^([a-z0-9-_]+)\.php$=DDi');
+define("WCOM_REGEX_FILE_NAME_PHP", '=^([a-z0-9-_]+)\.php$=Di');
 
 // Defines regular expression for strings containing
 // a group name
-define("WCOM_REGEX_GROUP_NAME", "=^(?!WCOM)([A-Z0-9]+)_([A-Z0-9_]+)$=DD");
+define("WCOM_REGEX_GROUP_NAME", "=^(?!WCOM)([A-Z0-9]+)_([A-Z0-9_]+)$=D");
 
 // Defines regular expression for strings containing 
 // only zero or one.
-define("WCOM_REGEX_ZERO_OR_ONE", "=^[0-1]$=DD");
+define("WCOM_REGEX_ZERO_OR_ONE", "=^[0-1]$=D");
 
 // Defines regular expression for strings containing
 // a user password
-define("WCOM_REGEX_PASSWORD", "=^\S{5,}$=DD");
+define("WCOM_REGEX_PASSWORD", "=^\S{5,}$=D");
 
 // Defines regular expression for strings containing
 // an email address
-define("WCOM_REGEX_EMAIL", "=^((\"[^\"\f\n\r\t\v\b]+\")|([\w\!\#\$\%\&\'\*\+\-\~\/\^\`\|\{\}]+(\.[\w\!\#\$\%\&\'\*\+\-\~\/\^\`\|\{\}]+)*))@((\[(((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9])))\])|(((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9])))|((([A-Za-z0-9\-])+\.)+[A-Za-z\-]+))$=DD");
+define("WCOM_REGEX_EMAIL", "=^((\"[^\"\f\n\r\t\v\b]+\")|([\w\!\#\$\%\&\'\*\+\-\~\/\^\`\|\{\}]+(\.[\w\!\#\$\%\&\'\*\+\-\~\/\^\`\|\{\}]+)*))@((\[(((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9])))\])|(((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9])))|((([A-Za-z0-9\-])+\.)+[A-Za-z\-]+))$=D");
 
 // Defines regular expression for strings containing a
 // right name
-define("WCOM_REGEX_RIGHT_NAME", "=^(?!WCOM)([A-Z0-9]+)_([A-Z0-9_]+)$=DD");
+define("WCOM_REGEX_RIGHT_NAME", "=^(?!WCOM)([A-Z0-9]+)_([A-Z0-9_]+)$=D");
 
 // Defines regular expression for strings containing a
 // ping service host
-define("WCOM_REGEX_PING_SERVICE_HOST", "=^([a-z0-9.-]+)$=DDi");
+define("WCOM_REGEX_PING_SERVICE_HOST", "=^([a-z0-9.-]+)$=Di");
 
 // Defines regular expression for strings containing a
 // ping service path
-define("WCOM_REGEX_PING_SERVICE_PATH", "=^/([a-z0-9.-\/]*)$=DDi");
+define("WCOM_REGEX_PING_SERVICE_PATH", "=^/([a-z0-9.-\/]*)$=Di");
 
 // Defines regular expression for strings containing an
 // internal text converter name
