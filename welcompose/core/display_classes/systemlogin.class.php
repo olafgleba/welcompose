31aa2b4af9e917494761731e6dab0709e4255ae5
Olaf Gleba <og@welcompose.de>
2007-06-23 21:43:09 +0000

    Issue with encoded ampersands in redirect fixed

--- welcompose/core/display_classes/systemlogin.class.php
+++ welcompose/core/display_classes/systemlogin.class.php
@@ -236,7 +236,7 @@ public function getMainTemplateName ()
  */
 public function getRedirectLocationSelf ()
 {
-	return "Location: ".$this->getLocationSelf();
+	return "Location: ".$this->getLocationSelf(true);
 }
 
 /**
