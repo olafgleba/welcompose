de77839468fa48a65ecc72c6d06433f84bc03b74
Olaf Gleba <og@welcompose.de>
2008-11-20 15:27:01 +0000

    Mod release.php footer to reflect agpl license

--- scripts/requirements.php
+++ scripts/requirements.php
@@ -387,9 +387,8 @@ if (!defined("PDO::ATTR_EMULATE_PREPARES")) {
 <!-- content --></div>
 
 <p class="copyright">
-<span class="fright">Licensed below the terms of the <a href="http://www.opensource.org/licenses/osl-3.0.php">Open 
-Software License 3.0</a></span>
-&copy; 2006 - 2007 <a href="http://www.welcompose.com/"><strong>Welcompose<sup>&reg;</sup></strong></a> Requirements Tester for 0.8 &ndash; powered by <a href="http://www.sopic.com/"><strong>Sopic<sup>&reg;</sup></strong></a>
+<span class="fright">Licensed below the terms of the <a href="http://www.opensource.org/licenses/agpl-v3.html">GNU AGPLv3</a></span>
+&copy; 2008 <a href="http://www.welcompose.de/"><strong>Welcompose<sup>&reg;</sup></strong></a> Requirements Tester for 0.8 &ndash; <a href="http://creatics.de/">creatics media.systems</a>
 </p><!-- container --></div>
 </body>
 </html>
