bac84b8f5fae4514d92d8385332e6f1a81628f3f
Olaf Gleba <og@welcompose.de>
2011-10-01 15:29:25 +0200

    Forgotten file related to ticket #18

---
+++ welcompose/admin/content/pages_apply_content.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+ * Project: Welcompose
+ * File: pages_apply_content.php
+ *
+ * Copyright (c) 2008 -2011 creatics
+ *
+ * Project owner:
+ * creatics, Olaf Gleba
+ * 50939 Köln, Germany
+ * http://www.creatics.de
+ *
+ * This file is licensed under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE v3
+ * http://www.opensource.org/licenses/agpl-v3.html
+ *
+ * @copyright 2011 creatics, Olaf Gleba
+ * @author Olaf Gleba
+ * @package Welcompose
+ * @license http://www.opensource.org/licenses/agpl-v3.html GNU AFFERO GENERAL PUBLIC LICENSE v3
+ */
+
+// define area constant
+define('WCOM_CURRENT_AREA', 'ADMIN');
+
+// get loader
+$path_parts = array(
+	dirname(__FILE__),
+	'..',
+	'..',
+	'core',
+	'loader.php'
+);
+$loader_path = implode(DIRECTORY_SEPARATOR, $path_parts);
+require($loader_path);
+
+// start base
+/* @var $BASE base */
+$BASE = load('base:base');
+
+// deregister globals
+$deregister_globals_path = dirname(__FILE__).'/../../core/includes/deregister_globals.inc.php';
+require(Base_Compat::fixDirectorySeparator($deregister_globals_path));
+
+// admin_navigation
+$admin_navigation_path = dirname(__FILE__).'/../../core/includes/admin_navigation.inc.php';
+require(Base_Compat::fixDirectorySeparator($admin_navigation_path));
+
+try {
+	// start output buffering
+	@ob_start();
+	
+	// load smarty
+	$smarty_admin_conf = dirname(__FILE__).'/../../core/conf/smarty_admin.inc.php';
+	$BASE->utility->loadSmarty(Base_Compat::fixDirectorySeparator($smarty_admin_conf), true);
+	
+	// load gettext
+	$gettext_path = dirname(__FILE__).'/../../core/includes/gettext.inc.php';
+	include(Base_Compat::fixDirectorySeparator($gettext_path));
+	gettextInitSoftware($BASE->_conf['locales']['all']);
+	
+	// start session
+	/* @var $SESSION session */
+	$SESSION = load('base:session');
+	
+	// load user class
+	/* @var $USER User_User */
+	$USER = load('user:user');
+	
+	// load login class
+	/* @var $LOGIN User_Login */
+	$LOGIN = load('User:Login');
+	
+	// load project class
+	/* @var $PROJECT Application_Project */
+	$PROJECT = load('application:project');
+	
+	// load page class
+	/* @var $PAGE Content_Page */
+	$PAGE = load('content:page');
+	
+	// init user and project
+	if (!$LOGIN->loggedIntoAdmin()) {
+		header("Location: ../login.php");
+		exit;
+	}
+	$USER->initUserAdmin();
+	$PROJECT->initProjectAdmin(WCOM_CURRENT_USER);
+	
+	// check access
+	if (!wcom_check_access('Content', 'Page', 'Manage')) {
+		throw new Exception("Access denied");
+	}
+	
+	// assign current user values
+	$_wcom_current_user = $USER->selectUser(WCOM_CURRENT_USER);
+	$BASE->utility->smarty->assign('_wcom_current_user', $_wcom_current_user);	
+	
+	// assign paths
+	$BASE->utility->smarty->assign('wcom_admin_root_www',
+		$BASE->_conf['path']['wcom_admin_root_www']);
+	
+	// assign current user and project id
+	$BASE->utility->smarty->assign('wcom_current_user', WCOM_CURRENT_USER);
+	$BASE->utility->smarty->assign('wcom_current_project', WCOM_CURRENT_PROJECT);
+		
+	// get page type from xhr
+	$type = Base_Cnc::filterRequest($_REQUEST['type'], WCOM_REGEX_NUMERIC);
+
+	// load pagetype class
+	$PAGETYPE = load('content:pagetype');
+	
+	// get page types by delivered type id
+	$pagetype = $PAGETYPE->selectPageType($type);
+		
+	/**
+	* Within the listed page types there
+	* is no content form field
+	**/	
+	if ($pagetype['name'] == 'WCOM_BLOG' || 
+			$pagetype['name'] == 'WCOM_URL' || 
+			$pagetype['name'] == 'WCOM_SIMPLE_DATE') {
+		$o .= '<option value="">'.gettext('There is no content available for this page type').'</option>';
+	} else {	
+		// get pages
+		$pages = $PAGE->selectPages();
+		
+		foreach ($pages as $_page) {	
+			switch((string)$_page['page_type_name']) {
+				case 'WCOM_GENERATOR_FORM':
+				case 'WCOM_SIMPLE_FORM':
+				case 'WCOM_SIMPLE_GUESTBOOK':
+				case 'WCOM_SIMPLE_PAGE':
+					$o .= '<option value="'.$_page['id'].'">'.$_page['name'].'</option>';
+				break;
+			}
+		}	
+	}
+
+	// xhr response
+	print $o;
+		
+	// flush the buffer
+	@ob_end_flush();
+	exit;
+
+} catch (Exception $e) {
+	// clean buffer
+	if (!$BASE->debug_enabled()) {
+		@ob_end_clean();
+	}
+	
+	// raise error
+	$BASE->error->displayException($e, $BASE->utility->smarty);
+	$BASE->error->triggerException($e);
+	
+	// exit
+	exit;
+}
+?>
