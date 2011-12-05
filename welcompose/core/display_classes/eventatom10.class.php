5e4f1b083138a24ef00e83683403afbe2aca4b2d
Olaf Gleba <og@welcompose.de>
2011-12-05 01:00:13 +0100

    Related to ticket 21 (add new page type event), display classes

---
+++ welcompose/core/display_classes/eventatom10.class.php
@@ -0,0 +1,250 @@
+<?php
+
+/**
+ * Project: Welcompose
+ * File: eventatom10.class.php
+ * 
+ * Copyright (c) 2008 creatics
+ * 
+ * Project owner:
+ * creatics, Olaf Gleba
+ * 50939 Köln, Germany
+ * http://www.creatics.de
+ *
+ * This file is licensed under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE v3
+ * http://www.opensource.org/licenses/agpl-v3.html
+ * 
+ * $Id$
+ * 
+ * @copyright 2011 creatics, Olaf Gleba
+ * @author Olaf Gleba
+ * @package Welcompose
+ * @license http://www.opensource.org/licenses/agpl-v3.html GNU AFFERO GENERAL PUBLIC LICENSE v3
+ */
+
+// load the display interface
+if (!interface_exists('Display')) {
+	$path_parts = array(
+		dirname(__FILE__),
+		'display.interface.php'
+	);
+	require(implode(DIRECTORY_SEPARATOR, $path_parts));
+}
+Event
+/**
+ * Class loader compatible to loader.php. Wrapps around constructor.
+ * 
+ * @param array
+ * @return object
+ */
+function Display_EventAtom10 ($args)
+{
+	// check input
+	if (!is_array($args)) {
+		trigger_error('Constructor args are not an array', E_USER_ERROR);
+	}
+	if (!array_key_exists(0, $args)) {
+		trigger_error('Constructor arg project does not exist', E_USER_ERROR);
+	}
+	if (!array_key_exists(1, $args)) {
+		trigger_error('Constructor arg page does not exist', E_USER_ERROR);
+	}
+
+	return new Display_EventAtom10($args[0], $args[1]);
+}
+
+class Display_EventAtom10 implements Display {
+
+	/**
+	 * Reference to base class
+	 *
+	 * @var object
+	 */
+	public $base = null;
+	
+	/**
+	 * Container for project information
+	 * 
+	 * @var array
+	 */
+	protected $_project = array();
+	
+	/**
+	 * Container for page information
+	 * 
+	 * @var array
+	 */
+	protected $_page = array();
+	
+	/**
+	 * Set appropriate content_type
+	 * 
+	 * @var string
+	 */
+	protected $_content_type = 'text/xml';
+	
+	/**
+	 * Set appropriate charset
+	 * 
+	 * @var string
+	 */
+	protected $_charset = 'utf-8';
+	
+/**
+ * Creates new instance of display driver. Takes an array
+ * with the project information as first argument, an array
+ * with the information about the current page as second
+ * argument.
+ * 
+ * @throws Display_EventAtom10Exception
+ * @param array Project information
+ * @param array Page information
+ */
+public function __construct($project, $page)
+{
+	try {
+		// get base instance
+		$this->base = load('base:base');
+		
+		// establish database connection
+		$this->base->loadClass('database');
+				
+	} catch (Exception $e) {
+		
+		// trigger error
+		printf('%s on Line %u: Unable to start base class. Reason: %s.', $e->getFile(),
+			$e->getLine(), $e->getMessage());
+		exit;
+	}
+	
+	// input check
+	if (!is_array($project)) {
+		throw new Display_EventAtom10Exception("Input for parameter project is expected to be an array");
+	}
+	if (!is_array($page)) {
+		throw new Display_EventAtom10Exception("Input for parameter page is expected to be an array");
+	}
+	
+	// assign project, page info to class properties
+	$this->_project = $project;
+	$this->_page = $page;
+}
+
+/**
+ * Default method that will be called from the display script
+ * and has to care about the page preparation. Returns boolean
+ * true on success.
+ * 
+ * @return bool
+ */ 
+public function render ()
+{
+	// assign current timestamp
+	$this->base->utility->smarty->assign('current_timestamp', time());
+	
+	// nothing to do
+	return true;
+}
+
+/**
+ * Returns the cache mode for the current template.
+ * 
+ * @return int
+ */
+public function getMainTemplateCacheMode ()
+{
+	return 0;
+}
+
+/**
+ * Returns the cache lifetime of the current template.
+ * 
+ * @return int
+ */
+public function getMainTemplateCacheLifetime ()
+{
+	return 0;
+}
+
+/** 
+ * Returns the name of the current template.
+ * 
+ * @return string
+ */ 
+public function getMainTemplateName ()
+{
+	// set headers for the feed
+	$this->setTemplateHeader();
+	
+	return "wcom:event_atom10.".WCOM_CURRENT_PAGE;
+}
+
+/**
+ * Returns the redirect location of the the current
+ * document (~ $PHP_SELF without it's problems) with the
+ * Location: header prepended.
+ * 
+ * @return string
+ */
+public function getRedirectLocationSelf ()
+{
+	return "Location: ".$this->getLocationSelf(true);
+}
+
+/**
+ * Returns the redirect location of the the current
+ * document (~ $PHP_SELF without it's problems). Already
+ * encoded ampersands will be removed if the optional
+ * parameter remove_amps is set to true.
+ * 
+ * @param bool Remove encoded ampersands
+ * @return string
+ */
+public function getLocationSelf ($remove_amps = false)
+{
+	// prepare params
+	$params = array(
+		'page_id' => $this->_page['id'],
+		'action' => 'Atom10'
+	);
+	
+	// send params to url generator. we hope to get back something useful.
+	$URLGENERATOR = load('Utility:UrlGenerator');
+	$url = $URLGENERATOR->generateInternalLink($params, $remove_amps);
+	
+	// return the url or a hash mark if the url is empty 
+	if (empty($url)) {
+		return '#';
+	} else {
+		return $url;
+	}
+}
+
+/**
+ * Returns appropriate header
+ * For example this should be used to asure valid feed output
+ * 
+ * @return string
+ */
+public function setTemplateHeader ()
+{
+	return header('Content-Type: '.$this->_content_type.'; charset='.$this->_charset.'');
+}
+
+/**
+ * Returns information whether to skip authentication
+ * or not.
+ * 
+ * @return bool
+ */
+public function skipAuthentication ()
+{
+	return false;
+}
+
+// end of class
+}
+
+class Display_EventAtom10Exception extends Exception { }
+
+?>
